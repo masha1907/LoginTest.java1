6810350fbd1771c4413c5a34bb0c62379b62f4cf
masha1907 <gottmann.MG@googlemail.com>
2022-08-18 17:04:57 +0200

    Test

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -3,45 +3,39 @@ package org.example;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.*;
 
 public class LoginTest {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @Before
-  public void setUp() {
-    System.setProperty("webdriver.edge.driver", "src\\main\\resources\\drivers 1\\msedgedriver.exe");
-    driver = new EdgeDriver();
-
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-  }
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void login() {
-    driver.get("https://the-internet.herokuapp.com/login");
-    driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).click();
-    driver.manage().window().setSize(new Dimension(1280, 796));
-    driver.findElement(By.id("username")).sendKeys("tomsmith");
-    driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-    driver.findElement(By.cssSelector(".fa")).click();
-    assertEquals(driver.findElement(By.cssSelector(".subheader")).getText(), "Welcome to the Secure Area.When you are done click logout below.");
+    private WebDriver driver;
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
 
 
-  }
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void Login() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.manage().window().setSize(new Dimension(1280, 796));
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector(".fa")).click();
+        Thread.sleep(3000);
+        assertEquals(driver.findElement(By.cssSelector(".icon-2x")).getText(), "Logout");
+
+    }
 }
